f6ae3d44579264634e761df2725cd84b54bfc5df
Yavuz <yavuzsamet6347@gmail.com>
2024-09-01 18:01:28 +0300

    Create Bolum ve Kalanı Bulma.cpp

---
+++ 2-Bölüm ve Kalanı Bulma/Bolum ve Kalanı Bulma.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int bolunen, bolen;
+
+	cout << "Bolunen sayiyi girin: ";
+	cin >> bolunen;
+
+	cout << "Bolen sayiyi girin: ";
+	cin >> bolen;
+
+	if (bolen != 0) {
+		int bolum = bolunen / bolen;
+		int kalan = bolunen % bolen;
+
+		cout << "Bolum: " << bolum << endl;
+		cout << "Kalan: " << kalan << endl;
+
+	}
+	else {
+		cout << "Bolen sifir olamaz!";
+	}
+
+	return 0;
+}
